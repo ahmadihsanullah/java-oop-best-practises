f541d7cdc5f3657d8dad5c554bbbcbafc01f4480
ahmadihsanullah <ahma21327ti@student.nurulfikri.ac.id>
2024-01-11 10:34:18 +0700

    Belajar Java OOP Best Practises

---
+++ src/com/tutorial/App.java
@@ -0,0 +1,9 @@
+package com.tutorial;
+import java.io.Serializable;
+
+public class App implements Serializable{
+    private String name;
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
